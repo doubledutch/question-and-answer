aca87c899fced2f1461cf136b4da038ddf886fe0
Adam Liechty <adam@doubledutch.me>
2018-02-01 13:08:34 -0800

    style fixes

--- web/admin/src/buttons.js
+++ web/admin/src/buttons.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import './App.css'
 
-export class CustomButtons extends Component {
+export default class CustomButtons extends Component {
   render() {
     const task = this.props.task 
     const header = this.props.header 
@@ -11,47 +11,47 @@ export class CustomButtons extends Component {
     
       if (header){
         return(
-        <span style={{marginTop: 25}}>
-          <span className='cellBoxRight'>
-            <img className='button1' onClick={()=>this.sendApprove(task)} src={require('./icons/checkocircle.svg')} alt="approve"/>
-            <img className='button1' onClick={()=>this.sendBlock(task)} src={require('./icons/deleteocircle.svg')} alt="block"/>
+          <span style={{marginTop: 25}}>
+            <span className='cellBoxRight'>
+              <img className='button1' onClick={()=>this.sendApprove(task)} src={require('./icons/checkocircle.svg')} alt="approve"/>
+              <img className='button1' onClick={()=>this.sendBlock(task)} src={require('./icons/deleteocircle.svg')} alt="block"/>
+            </span>
           </span>
-        </span>
         )
       }
       
       if (approve){
         if (pin){
           return(
-          <span style={{marginTop: 25}}>
-            <span className='cellBoxRight'>
-              <img className='button1' onClick={()=>this.sendAnswer(task)} src={require('./icons/check.svg')} alt="answered"/>
-              <img className='button1' onClick={()=>this.sendBlock(task)} src={require('./icons/deleteocircle.svg')} alt="block"/>
+            <span style={{marginTop: 25}}>
+              <span className='cellBoxRight'>
+                <img className='button1' onClick={()=>this.sendAnswer(task)} src={require('./icons/check.svg')} alt="answered"/>
+                <img className='button1' onClick={()=>this.sendBlock(task)} src={require('./icons/deleteocircle.svg')} alt="block"/>
+              </span>
             </span>
-          </span>
           )
         }
         else {
           return(
-          <span>
-            <button className="pinButton" onClick={()=>this.sendPin(task)}><img className='pinImage' onClick={()=>this.sendPin(task)} src={require('./icons/thumbtack.svg')} alt="pin to top" />Pin to Top</button>
-            <span className='cellBoxRight'>
-              <img className='button1' onClick={()=>this.sendAnswer(task)} src={require('./icons/check.svg')} alt="answered"/>
-              <img className='button1' onClick={()=>this.sendBlock(task)} src={require('./icons/deleteocircle.svg')} alt="block"/>
+            <span>
+              <button className="pinButton" onClick={()=>this.sendPin(task)}><img className='pinImage' onClick={()=>this.sendPin(task)} src={require('./icons/thumbtack.svg')} alt="" />Pin to Top</button>
+              <span className='cellBoxRight'>
+                <img className='button1' onClick={()=>this.sendAnswer(task)} src={require('./icons/check.svg')} alt="answered"/>
+                <img className='button1' onClick={()=>this.sendBlock(task)} src={require('./icons/deleteocircle.svg')} alt="block"/>
+              </span>
             </span>
-          </span>
           )
         }
       }
 
       if (block){
         return(
-        <span style={{marginTop: 25}}>
-          <span className='cellBoxRight'>
-            <img className='button1' onClick={()=>this.sendApprove(task)} src={require('./icons/checkocircle.svg')} alt="approve" />
-            <img className='button1' src={require('./icons/deletecircle.svg')} />
+          <span style={{marginTop: 25}}>
+            <span className='cellBoxRight'>
+              <img className='button1' onClick={()=>this.sendApprove(task)} src={require('./icons/checkocircle.svg')} alt="approve" />
+              <img className='button1' src={require('./icons/deletecircle.svg')} alt="" />
+            </span>
           </span>
-        </span>
         )
       }
     
@@ -71,7 +71,4 @@ export class CustomButtons extends Component {
     sendPin(task){
       this.props.makePin(task)
     }
-
 }
-
-export default CustomButtons
