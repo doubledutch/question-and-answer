2101b3784a46b2ac1297baf350f24065d9f5f767
Adam Liechty <adam@doubledutch.me>
2018-03-23 21:35:00 -0700

    Separate catch from this.signin

--- mobile/src/home-view.js
+++ mobile/src/home-view.js
@@ -60,7 +60,8 @@ class HomeView extends Component {
     }
     this.signin = fbc.signin()
       .then(user => this.user = user)
-      .catch(err => console.error(err))
+
+    this.signin.catch(err => console.error(err))
   }
 
   componentDidMount() {
