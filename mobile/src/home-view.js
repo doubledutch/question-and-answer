77508bbea8d2ff01af2cd6e7fb5bf45b333c86aa
Eddie Stamps <estamps@doubledutch.me>
2018-06-11 20:41:11 +0000

    PR

--- mobile/src/home-view.js
+++ mobile/src/home-view.js
@@ -271,10 +271,10 @@ class HomeView extends Component {
         this.originalOrder(otherQuestions)
         const allQuestions = pinnedQuestions.concat(otherQuestions)
         let orderedQuestions = allQuestions
-        if (currentSort === "Answered") orderedQuestions = allQuestions.filter(item => item.answered === true && item.session === session.key)
-        if (currentSort === "Blocked") orderedQuestions = allQuestions.filter(item => item.block === true && item.new === false && item.session === session.key)
-        if (currentSort === "New") orderedQuestions = allQuestions.filter(item => item.approve === false && item.new === true && item.session === session.key)
-        else orderedQuestions = allQuestions.filter(item => item.block === false && item.answered === false && item.approve && item.new === false && item.session === session.key)
+        if (currentSort === "Answered"){ orderedQuestions = allQuestions.filter(item => item.answered === true && item.session === session.key) }
+        if (currentSort === "Blocked"){ orderedQuestions = allQuestions.filter(item => item.block === true && item.new === false && item.session === session.key) }
+        if (currentSort === "New"){ orderedQuestions = allQuestions.filter(item => item.approve === false && item.new === true && item.session === session.key) }
+        else { orderedQuestions = allQuestions.filter(item => item.block === false && item.answered === false && item.approve && item.new === false && item.session === session.key) }
         return orderedQuestions
       }
       else {
